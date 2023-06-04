b1b007c64bc68bdba4346a055fff539af9f8275a
Sefat Anam <sefatanam@gmail.com>
2023-06-04 16:38:14 +0600

    fix: swagger docs

--- libs/products/src/lib/products.controller.ts
+++ libs/products/src/lib/products.controller.ts
@@ -3,12 +3,19 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ProductDto } from './dtos/product.dto';
 
 @ApiTags('Products')
@@ -16,23 +23,42 @@ import { ProductDto } from './dtos/product.dto';
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @ApiOperation({ description: 'Get all product which are not disable.' })
+  @ApiResponse({
+    description: `Get all product which are not disable.`,
+    status: HttpStatus.OK,
+    type: ProductDto,
+  })
   @Get()
   public async getProducts() {
     return await this.productsService.getProducts();
   }
 
+  @ApiOperation({ description: 'Get a product which is not disable.' })
   @ApiParam({ name: 'id', type: String })
   @Get(':id')
   public async getProduct(@Param('id') id: string) {
     return await this.productsService.getProduct(id);
   }
 
+  @ApiOperation({ description: 'Create product' })
+  @ApiResponse({
+    description: `Create product`,
+    status: HttpStatus.OK,
+    type: ProductDto,
+  })
   @ApiBody({ type: ProductDto })
   @Post()
   public async createProduct(@Body() product: ProductDto) {
     return this.productsService.createProduct(product);
   }
 
+  @ApiOperation({ description: 'Update product by uuid' })
+  @ApiResponse({
+    description: `Update product`,
+    status: HttpStatus.OK,
+    type: ProductDto,
+  })
   @ApiBody({ type: ProductDto })
   @Patch(':id')
   public async updateProduct(
@@ -42,6 +68,7 @@ export class ProductsController {
     return this.productsService.updateProduct(id, product);
   }
 
+  @ApiOperation({ description: 'Delete product by product uuid' })
   @ApiParam({ name: 'id', type: String })
   @Delete(':id')
   public async deleteProduct(@Param('id') id: string) {

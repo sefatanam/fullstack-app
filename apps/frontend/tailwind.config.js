63a2719a0bef6894533fcf4cc83142b48a4b7723
Sefat Anam <sefatanam@gmail.com>
2023-06-11 01:18:58 +0600

    feat: tailwindcss setup locally

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Customize your colors here
+        'white':'#ECF0F1',
+        'blue':'#3498DB',
+        'primary': '#2C3E50',
+        'accent': '#E74C3C',
+        'error':'#e70e41',
+      },
+    },
+  },
+  plugins: [],
+};

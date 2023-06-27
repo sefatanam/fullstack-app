ce0cb1c1b249c5212b96367d69a6f85189e51259
Sefat Anam <sefatanam@gmail.com>
2023-06-28 03:02:16 +0600

    feat: add angular material

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" })
+    RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
